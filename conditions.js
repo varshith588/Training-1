2a4d3d9bafbc95c9c424edc6dc3aef622982e1b5
varshith588 <varshithreddychintala@gmail.com>
2025-01-07 18:25:59 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,80 @@
+// if(true){
+//     console.log("If block executing");
+// }
+// if(false){
+//     console.log("If block executing");
+// }
+
+// let age = 17;
+// if(age >=18){
+//     console.log("eligible for voting");
+// }else{
+//     console.log("Not eligible for voting");
+// }
+
+// age >= 18 ? console.log("Major"):console.log("Minor");
+
+// let day = prompt("Enter a day here");
+
+// if(day == 1){
+//     console.log("Sun");
+// }
+// else if(day == 2){
+//     console.log("Mon");
+// }
+// else if(day == 3)
+// {
+//     console.log("Tue");
+// }
+// else if(day == 4)
+// {
+//     console.log("Wed");
+// }else if(day == 5)
+// {
+//     console.log("Thu");
+// }
+// else if(day == 6)
+// {
+//     console.log("Fri");
+// }else if(day == 7){
+//     console.log("Sat");
+// }
+// else{
+//     console.log("Invalid Day");
+// }
+
+// let day = Number(prompt("Enter a day here"));
+// console.log(day , typeof(day));
+
+// switch (day) {
+//         case 1:
+//         console.log("Sun");
+//         break;
+//         case 2:
+//         console.log("Mon");
+//         break;
+//         case 3:
+//         console.log("Tue");
+//         break;
+//         case 4:
+//         console.log("Wed");
+//         break;
+//         case 5:
+//         console.log("Thu");
+//         break;
+//         case 6:
+//         console.log("Fri");
+//         break;
+//         case 7:
+//         console.log("Sat");
+//         break;
+//     default:console.log("Invalid day");
+//         break;
+// }
+
+//!take 3 values and print which value is gretaer among the three values.
+//! let a =20;
+//! let b=140;
+//! let c= 100;
+
+//! do this with both if-else ladder and switch condition
---
+++ operators.js
@@ -0,0 +1,80 @@
+// ! arithmatic operator
+// console.log(10 + 10);//! addition 
+// console.log("10" + 10);//!1010 concetation 
+// console.log(10 + 40 + "Hi");//!50Hi
+
+// console.log("JS"+50+50);//!JS5050
+// console.log("JS"+(50+50));//! JS100
+
+// console.log(10-5);
+// console.log("10" - 5);//! typecasting
+// console.log("10" - "hi");//! NaN
+// console.log("10" - "5");
+
+// console.log(5*5);
+// console.log("5"*5);//! typecasting
+
+// console.log(10/2);
+// console.log("10"/2);//! typecasting
+
+
+// console.log(10 % 2);//! 
+// console.log("10"% "2");//! typecasting
+
+// console.log(2**5);
+// console.log("2"**5);//! typecasting
+
+// let a =10;
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+
+// console.log(--a);//! 9
+// console.log(a)
+
+//!assignment operator:-
+// let a = 10;
+// console.log(a);
+// console.log(a + 5);
+// console.log(a+5);
+// console.log(a+=5);//! a = a+5 = 15
+// console.log(a+=5);
+// console.log(a-=5);//! a= a-5 = 5
+// console.log(a*=5);//! 50
+// console.log(a/=5);//
+// console.log(a%=5);
+// console.log(a**=2);
+// console.log(a);
+
+// ! relational operators
+
+// console.log(10 == 10);
+// console.log("Hi" == "Hi");
+// console.log("10" == 10);
+
+// console.log(10 === 10);
+// console.log("10" === 10);
+
+// console.log(10 != 10);
+// console.log("10" != 10);
+// console.log(100 != 10);
+
+// console.log(10 !== 10);
+// console.log("10" !== 10);
+
+// console.log(10 > 5);
+// console.log(10 >= 10);
+// console.log(10 < 20);
+// console.log(20 <= 20);
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+// console.log(!true);
+// console.log(!false);
