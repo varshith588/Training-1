8d64b99a1193de72de50417da85eb5463f6b6d89
varshith588 <varshithreddychintala@gmail.com>
2025-01-08 10:27:03 +0530

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,48 @@
+// console.log("Good morning")
+// console.log("Good morning")
+// console.log("Good morning")
+// console.log("Good morning")
+// console.log("Good morning")
+
+//! for loop
+// for(let i=1;i<5;i++){
+//     console.log("Good morning");
+// }
+
+// let n =100;
+// for(let i = 0; i<=n;i++){
+//     console.log(i);
+// }
+
+// 1 is a odd Number
+// 2 is a even Number
+// 3 is a odd Number
+// let n =100;
+// for( let i = 0; i<=n;i++){
+//     if( i%2!==0){
+//         console.log(i);
+//         console.log("is an odd number");
+//     }
+//     else{
+//         console.log(i);
+//         console.log("is an even number");
+
+//     }
+// }
+
+// let i=1;
+// while(i <=5){
+//     console.log("good morning!!!");
+//     i++;
+// }
+
+// for(let i =1; i<=10;i++){
+//     console.log(i);
+//     if(i == 5) break;
+// }
+
+
+for(let i =1; i<=10;i++){
+    if(i == 5) continue;
+    console.log(i);
+}
