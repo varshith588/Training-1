20e0fb7a47be89d7f6c57759e78e91d56f7832ee
varshith588 <varshithreddychintala@gmail.com>
2025-01-09 22:41:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,68 @@
+// function addToCart(){
+//     alert("Product is added to cart")
+// }
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10 , 40)
+// add(30 , 40)
+// add(50 , 40)
+// add(100 , 40)
+// add(13 , 50)
+
+// ! function without parameters
+// function demo(){
+//     console.log("function without parameters");
+// }
+// demo();
+
+// ! function with parameters
+
+// function userDetails(name , age , city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("Yeshwant" , 20 , "Hyderbad");
+
+//! anonymus function
+// function(){
+//     console.log("Anonymus function");
+// }
+// ();
+
+// ! function  expression
+// let x = function(){
+//     console.log("function expression is executing");
+// };
+// x();
+
+
+// ! IIFE
+// (function(){
+//     console.log("IIFE");
+// })();
+
+//! arrow function
+// function demo() 
+// {
+//     console.log("Hello");
+// }
+// demo();
+
+// let x = ()=>console.log("Arrow function");
+// x();
+
+// let x =_=>console.log("Arrow function");
+// x();
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x =(a , b)=>console.log(a+b);
+// x(5,5);
+// let x=_=>{console.log("Hi");
+//         console.log("Hello");
+//         console.log("Bye");}
+//     x();
