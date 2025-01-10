bf0ee2a56502b5e3ab7593d5dda87ef97c8e676f
varshith588 <varshithreddychintala@gmail.com>
2025-01-10 22:50:24 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -66,3 +66,49 @@
 //         console.log("Hello");
 //         console.log("Bye");}
 //     x();
+
+//! return keyword
+// function demo(a,b){
+//    return a+b;//! explicit return
+// };
+// let x = demo(5,5);
+// console.log(x);
+
+// let y = (a,b) => a+b;//! implicit return
+// let a = y(10,20);
+// console.log(a);
+
+// let z = (a,b)=>{return a+b};//! explicit return
+// let b = z(10,100);
+// console.log(b);                     
+
+//! Higher oreder function
+//  function hof(a){
+//     return a;
+//  };
+//  let x = hof(function(){return "hello  i am a callback function"});
+//  console.log(x);
+
+// function parent(callback){ // higher order function
+//     return callback
+// }
+//  let x = parent(add(5,5));
+// confirm.log(x);
+
+// function add(a,b){ // callback  function
+//     return a+b
+// }
+// console.log(add(5,5));
+
+
+// // closure
+// function x(){
+//     var user = "Vikas";
+//     let company = "Google";
+//     const sal = 1234567890;
+//     console.log(user);
+//     console.log(company);
+//     console.log(sal);
+//     console.log(a , b);
+// }
+// x();
