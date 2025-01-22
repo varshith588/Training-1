3ce6b11e9ac092a458374862e6ec73ab239a7a5c
varshith588 <varshithreddychintala@gmail.com>
2025-01-22 12:57:09 +0530

    Add files via upload

--- Array.js
+++ Array.js
@@ -36,15 +36,58 @@
 
 //! JSON
 
-let userDetails ={
-    name: "Sam",
-    age: 25,
-    city:"Hyd"
-};
-    console.log(userDetails);
+// let userDetails ={
+//     name: "Sam",
+//     age: 25,
+//     city:"Hyd"
+// };
+//     console.log(userDetails);
     
-    let x = JSON. stringify(userDetails);
-    console.log("JSON object "+x);
+//     let x = JSON. stringify(userDetails);
+//     console.log("JSON object "+x);
     
-    let y = JSON.parse(x);
-    console. log(y);
+//     let y = JSON.parse(x);
+//     console. log(y);
+
+//!api  fetching
+//! https://jsonplaceholder.typicode.com/users
+
+// function fetchUsers()
+// {
+//     let response = fetch(" https://jsonplaceholder.typicode.com/users");
+//     //console.log(response);
+//     response.then(result=>{
+//        // console.log(res.json());
+//        return result.json().then(data=>{
+//         console.log(data);
+//        })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchUsers();
+function fetchUsers() {
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    response
+      .then((result) => {
+        // console.log(result.json());
+        return result.json().then((data) => {
+          // console.log(data);
+          let store = document.getElementById("store");
+          // console.log(store);
+          data.map((user) => {
+            // console.log(user);
+            store.innerHTML += `
+            <tr>
+              <td>${user.id}</td>
+              <td>${user.name}</td>            
+              <td>${user.email}</td>
+              <td>${user.company.name}</td>
+            </tr>
+            `;
+          });
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  fetchUsers();
