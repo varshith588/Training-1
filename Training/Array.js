92f174b1fe9b2e251188aa16257770d9f15013a4
varshith588 <varshithreddychintala@gmail.com>
2025-01-22 12:54:34 +0530

    Add files via upload

---
+++ Training/Array.js
@@ -0,0 +1,93 @@
+// const user1 = "Sathvik";
+// const user2 =  "Teju";
+// const user3 = "Vishu";
+// const user4 = "Pavithra";
+// const user5 = "Raghu";
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+//const user = ["Shravan" , "Sathvik" , "Raghu", "Teju", "Vishnu"]
+
+// let users = ["Ramaya" , "SaiSmitha", "Rahul", "Prabhas","NTR"];
+// //let x = users.map((user)=>{
+// //    return user;
+// // })
+// //console.log(x);
+
+// let x = users.forEach((user)=>{
+//     //console.log(user);
+//     return user;
+// });
+// console.log(x); // !undefined
+
+//! create array of objects 
+// let usersDetails = {
+//     name : "Varshith",
+//     age:21,
+//     company : "IBM",
+//     sal : 7465,
+//     details:function(){
+//         return  `My anme is ${this.name} and i am working in ${thiis.company}`
+//     }
+// }
+
+//! JSON
+
+// let userDetails ={
+//     name: "Sam",
+//     age: 25,
+//     city:"Hyd"
+// };
+//     console.log(userDetails);
+    
+//     let x = JSON. stringify(userDetails);
+//     console.log("JSON object "+x);
+    
+//     let y = JSON.parse(x);
+//     console. log(y);
+
+//!api  fetching
+//! https://jsonplaceholder.typicode.com/users
+
+// function fetchUsers()
+// {
+//     let response = fetch(" https://jsonplaceholder.typicode.com/users");
+//     //console.log(response);
+//     response.then(result=>{
+//        // console.log(res.json());
+//        return result.json().then(data=>{
+//         console.log(data);
+//        })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchUsers();
+function fetchUsers() {
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    response
+      .then((result) => {
+        // console.log(result.json());
+        return result.json().then((data) => {
+          // console.log(data);
+          let store = document.getElementById("store");
+          // console.log(store);
+          data.map((user) => {
+            // console.log(user);
+            store.innerHTML += `
+            <tr>
+              <td>${user.id}</td>
+              <td>${user.name}</td>            
+              <td>${user.email}</td>
+              <td>${user.company.name}</td>
+            </tr>
+            `;
+          });
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  fetchUsers();
---
+++ Training/function.js
@@ -0,0 +1,151 @@
+// function addToCart(){
+//     alert("Product is added to cart")
+// }
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10 , 40)
+// add(30 , 40)
+// add(50 , 40)
+// add(100 , 40)
+// add(13 , 50)
+
+// ! function without parameters
+// function demo(){
+//     console.log("function without parameters");
+// }
+// demo();
+
+// ! function with parameters
+
+// function userDetails(name , age , city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// userDetails("Yeshwant" , 20 , "Hyderbad");
+
+//! anonymus function
+// function(){
+//     console.log("Anonymus function");
+// }
+// ();
+
+// ! function  expression
+// let x = function(){
+//     console.log("function expression is executing");
+// };
+// x();
+
+
+// ! IIFE
+// (function(){
+//     console.log("IIFE");
+// })();
+
+//! arrow function
+// function demo() 
+// {
+//     console.log("Hello");
+// }
+// demo();
+
+// let x = ()=>console.log("Arrow function");
+// x();
+
+// let x =_=>console.log("Arrow function");
+// x();
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x =(a , b)=>console.log(a+b);
+// x(5,5);
+// let x=_=>{console.log("Hi");
+//         console.log("Hello");
+//         console.log("Bye");}
+//     x();
+
+//! return keyword
+// function demo(a,b){
+//    return a+b;//! explicit return
+// };
+// let x = demo(5,5);
+// console.log(x);
+
+// let y = (a,b) => a+b;//! implicit return
+// let a = y(10,20);
+// console.log(a);
+
+// let z = (a,b)=>{return a+b};//! explicit return
+// let b = z(10,100);
+// console.log(b);                     
+
+//! Higher oreder function
+//  function hof(a){
+//     return a;
+//  };
+//  let x = hof(function(){return "hello  i am a callback function"});
+//  console.log(x);
+
+// function parent(callback){ // higher order function
+//     return callback
+// }
+//  let x = parent(add(5,5));
+// confirm.log(x);
+
+// function add(a,b){ // callback  function
+//     return a+b
+// }
+// console.log(add(5,5));
+
+
+// // closure
+// function x(){
+//     var user = "Vikas";
+//     let company = "Google";
+//     const sal = 1234567890;
+//     console.log(user);
+//     console.log(company);
+//     console.log(sal);
+//     console.log(a , b);
+// }
+// x();
+
+// function x(){
+//     let x = 10;
+//     var y = 20;
+//     console.log(x, y);
+// }
+// x();
+// console.log(x);
+
+// function x() {
+//     let a = 10;
+//     const b = 20;
+//     console.log(a,b);
+//     function z(){
+//         let p="Js";
+//         var q = "ReactJs";
+//         console.log(p, q);
+//         function z(){
+//             const username ="Harish";
+//             console.log(username);
+//             console.log(a);
+//             console.log(q);
+//         }
+//         z();
+//     }
+//     y();
+// }
+// x();
+var a =100;
+let b="hi";
+function x(){
+    var user = "Vinay";
+    let company = "Google";
+    const sal = 1234567890;
+    
+};
+x();
