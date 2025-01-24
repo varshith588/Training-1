78608f67f6a60190d65c00bce5f580b7b26d4ce1
varshith588 <varshithreddychintala@gmail.com>
2025-01-24 21:18:05 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -5,7 +5,36 @@
 // element.innerText = "G+"
 // console.log(element);
 
-let ele = document.getElementById("test");
-// ele.innerText= "<h1>Header</h1>"
-ele.innerHTML = "<h1>Header</h1>"
-console.log(ele);
+// let ele = document.getElementById("test");
+//  ele.innerText= "<h1>Header</h1>"
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+
+//!
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// // ele[0].style.backgroundColor="tomato"
+
+// //!spread operator :- it will takeout each and every value from the orginal array and store it inside one more new array(pure array).
+// //? syntax :- [...variable];
+// let x = [...ele];
+// console.log(x , Array.isArray(x));
+// x.map((element) => {
+// //console.log(element);
+// element.style.backgroundColor = "gold";
+// })
+
+//!
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele)
+
+let ele = document.querySelector("#btn");
+//! addEventListner("event" ,()=>{})
+    BigInt.addEventListner("click",()=>{
+        console.log("button clicked");
+        alert("hi")
+        document.body.style.backgroundColor="red"
+    })
