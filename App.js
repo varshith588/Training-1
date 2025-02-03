8374c6c90774581977f0cbfb9908760d9838f70a
varshith588 <varshithreddychintala@gmail.com>
2025-02-03 13:02:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+// function App() {
+//   // let ele = React.createElement(
+//   //   "div",
+//   //   { className: "App" },
+//   //   React.createElement("h1", null, "Header")
+//   // );
+//   let username = "Rohan";
+//   return (
+//     // <div className="App">
+//     //   <h1 style={{ color: "red" }}>Name: Subhapreet Patro</h1>
+//     //   <input />
+//     // </div>
+//     // <section>
+//     //   <h1>One JSX Element</h1>
+//     //   <p>lorem3</p>
+//     // </section>
+//     <div>
+//       <h1>{username}</h1>
+//       <p>{100 + 100}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ! Components
+// ? CBC
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Class based Component</h1>;
+//   }
+// }
+
+// export default App;
+
+// ? FBC
+// function App() {
+//   return <h1>Functional Based Component</h1>;
+// }
+// export default App;
+
+// OR
+
+const App = () => {
+  return <h1>Functional based Component</h1>;
+};
+export default App;
